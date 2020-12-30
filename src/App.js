848c3eb51af153c75b1df190c4811976ecf86419
Kishan Kumar Hs <kishankumar@users.noreply.github.com>
2020-12-30 13:56:05 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,387 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {
+  List,
+  Card,
+  Select,
+  Typography,
+  Row,
+  Col,
+  Button,
+  Avatar,
+  Space,
+  Divider,
+} from "antd";
+import {
+  CalendarOutlined,
+  EyeOutlined,
+  StarFilled,
+  FieldTimeOutlined,
+  AimOutlined,
+  InfoCircleOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
+import "antd/dist/antd.css";
+import data from "./components/data";
+import React from "react";
+import Meta from "antd/lib/card/Meta";
 function App() {
+  const { Text, Title } = Typography;
+  const { Option } = Select;
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+  const CostomHeder = (props) => {
+    return (
+      <Row
+        className="main-header"
+        style={{ paddingLeft: "20px" }}
+        gutter={[16, 24]}
+      >
+        <Col span={24}>
+          <Text style={{ color: "#000" }}>
+            Search results from
+            <Text
+              strong
+              style={{
+                color: "salmon",
+              }}
+            >
+              "Dos in All Catagoreis"
+            </Text>
+            &nbsp; with in
+            <Text
+              strong
+              style={{
+                color: "salmon",
+              }}
+            >
+              &nbsp; "15 Kilometers"
+            </Text>
+          </Text>
+        </Col>
+        <Row justify="space-between" gutter={[16, 24]}>
+          <Col
+            span={12}
+            className="items"
+            style={{
+              display: " flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              block
+              type="text"
+              style={{
+                background: "transparent",
+                width: "100px",
+              }}
+            >
+              Shopes
+            </Button>
+            <Button
+              block
+              type="text"
+              style={{
+                borderRadius: "10px",
+                background: "rgb(248, 167, 17)",
+                width: "100px",
+                borderColor: "rgb(248, 167, 17)",
+              }}
+            >
+              Items
+            </Button>
+          </Col>
+          <Col
+            span={12}
+            className="sortingContainer"
+            style={{
+              alignItems: "self-end",
+              justifyContent: "right",
+            }}
+          >
+            <Select
+              defaultValue="sort by"
+              className="sorting"
+              style={{ width: 150 }}
+              onChange={props.handleChange}
+            >
+              <Option value="kms">keliometer</Option>
+              <Option value="ratings">ratings</Option>
+              <Option value="price">Price</Option>
+            </Select>
+          </Col>
+        </Row>
+      </Row>
+    );
+  };
+  // const Newfotter = (props) => {
+  //   return (
+  //     <footer className="page-footer">
+  //       <p>
+  //         &copy; All Rights are Reserved by <a href="#">Mnemonicx</a>
+  //       </p>
+  //     </footer>
+  //   );
+  // };
+  const renderItem = (item) => {
+    const titleStyle = {
+      fontSize: ScWidth <= 700 ? "13px" : "1.1rem",
+      fontWeight: ScWidth <= 700 ? "300" : "bold",
+    };
+    return (
+      <section className="FoodsHolder" key={item.key}>
+        <Row
+          style={{
+            background: "#FBE9E7",
+          }}
+        >
+          <Col
+            xl={{ span: 11, offset: 1 }}
+            lg={{ span: 11, offset: 1 }}
+            md={19}
+            sm={19}
+            className="FoodsMainArea"
+            style={{ alignItems: "center", justifyContent: "flex-end" }}
+          >
+            <div style={{ width: "100%" }}>
+              <Text strong style={titleStyle}>
+                {item.title.toUpperCase()}
+              </Text>
+            </div>
+            <Space
+              split={
+                <Divider type="vertical" style={{ backgroundColor: "#000" }} />
+              }
+            >
+              <Text
+                strong
+                style={{
+                  color: "#F66574",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                }}
+              >
+                {item.status.toUpperCase()}
+              </Text>
+              <Text
+                strong
+                style={{
+                  color: "#0882C5",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                }}
+              >
+                {item.offers.toUpperCase()}
+              </Text>
+              {/* <Text strong style={{color:"#F66574",fontSize:"16px",textTransform:'uppercase'}} >{item.status}</Text> */}
+            </Space>
+            <br />
+            <Space
+              split={
+                <Divider type="vertical" style={{ backgroundColor: "#000" }} />
+              }
+            >
+              <Text
+                strong
+                style={{
+                  color: "#FF7D4C",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                }}
+              >
+                <StarFilled /> &nbsp;
+                {item.ratings <= 0 ? (
+                  <Text
+                    style={{
+                      color: "#FF7D4C",
+                      fontSize: "12px",
+                      textTransform: "uppercase",
+                    }}
+                    strong
+                  >
+                    NO RATINGS
+                  </Text>
+                ) : (
+                  <Text
+                    style={{
+                      color: "#FF7D4C",
+                      fontSize: "12px",
+                      textTransform: "uppercase",
+                    }}
+                    strong
+                  >
+                    {item.ratings}
+                  </Text>
+                )}
+              </Text>
+              <Text
+                strong
+                style={{
+                  color: "#000",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                }}
+              >
+                <FieldTimeOutlined /> &nbsp;
+                {item.opnings}
+              </Text>
+              <Text
+                strong
+                style={{
+                  color: "#000",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                }}
+              >
+                <AimOutlined />
+                &nbsp;
+                {item.kms}
+              </Text>
+              <Text
+                strong
+                style={{
+                  color: "#000",
+                  fontSize: "12px",
+                  textTransform: "uppercase",
+                }}
+              >
+                <InfoCircleOutlined />
+                &nbsp;
+                {item.delivery.toUpperCase()}
+              </Text>
+            </Space>
+          </Col>
+          <Col
+            className="foodBtnHolder"
+            xl={{ span: 11, offset: 1 }}
+            lg={{ span: 11, offset: 1 }}
+            md={5}
+            sm={5}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-around",
+            }}
+          >
+            <Button
+              className="viewAllBtn"
+              type="link "
+              size={window.innerWidth >= 600 ? "small" : "middle"}
+              style={{
+                color: "#000",
+              }}
+              icon={<EyeOutlined />}
+            >
+              {window.innerWidth <= 700 ? "" : "VIEW ALL ITEMS"}
+            </Button>
+            <Button
+              style={{ background: "#FBE9E7" }}
+              className="bookBtn"
+              icon={<CalendarOutlined />}
+            >
+              {window.innerWidth <= 700 ? "" : "BOOK APPONTMENT"}
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            {item.foods.map((i) => (
+              <Card bordered={false} style={{ width: "100%" }}>
+                <Space align="start"
+                style={{width:"50%"}}
+                split={''}
+                >
+                  <Meta
+                    avatar={<Avatar shape="square" size={60} src={i.imgUrl} />}
+                   
+                  />
+                  <div style={{width:"100%"}}>
+                  <Text strong style={titleStyle}>{i.foodTitle.toUpperCase()}</Text>
+                  </div>
+                  <div>
+                    {i.variant
+                      ? i.variant.map((e) => (
+                          <span
+                            className="vari"
+                            style={{
+                              background: "#EBF6FC",
+                              color: "#1D7CD1",
+                              fontWeight: "bold",
+                              fontSize:
+                                window.innerWidth <= 600 ? "10px" : "small",
+                              padding: "2px",
+                              margin: "2px",
+                              borderWidth: "1px",
+                              borderColor: "#1D7CD1",
+                            }}
+                          >
+                            {e.toUpperCase()}
+                          </span>
+                        ))
+                      : null}
+                  </div>
+                </Space>
+
+                <Space
+                  style={{
+                    justifyContent: "space-between",
+                    alignItems: "flex-end",
+                    width: "50%",
+                  }}
+                  align="center"
+                  className="InnerCardSection"
+                >
+                  <Text strong> RS &nbsp;{i.price}</Text>
+                  <Button
+                    type="primary"
+                    style={{ backgroundColor: "#AAA", borderColor: "#aaa" }}
+                    icon={<PlusOutlined />}
+                  >
+                    {window.innerWidth <= 700 ? "" : "ADD"}
+                  </Button>
+                </Space>
+              </Card>
+            ))}
+          </Col>
+        </Row>
+      </section>
+    );
+  };
+  const [ScWidth, setScWidth] = React.useState(window.innerWidth);
+  React.useEffect(() => {
+    setScWidth(window.innerWidth);
+  }, [ScWidth]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          alignContent: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <List
+          grid={{
+            gutter: 16,
+            lg: 1,
+            xl: 1,
+            md: 1,
+            sm: 1,
+            xs: 1,
+            xxl: 1,
+          }}
+          style={{ paddingLeft: "20px", paddingRight: "20px" }}
+          header={<CostomHeder handleChange={handleChange} />}
+          // footer={<Newfotter />}
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={renderItem}
+        />
+      </div>
     </div>
   );
 }
