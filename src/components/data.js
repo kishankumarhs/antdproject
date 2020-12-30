0b005dc9447dbf1fad7cab179af34e8c0c2dbc96
Kishan Kumar Hs <kishankumar@users.noreply.github.com>
2020-12-30 14:01:38 +0530

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,77 @@
+const url = "https://srv-store6.gofile.io/download/DzdM1h/Black%20and%20Orange%20Barbell%20Sports%20Logo%20(1).png"
+ const data=[
+    {
+        title:'mobile food courts',
+        status:"currently offline",
+        ratings:0,
+        opnings:'4 Hrs',
+        offers:'Rs 100 CashBack',
+        kms:6.1,
+        delivery:'free delivery',
+        foods:[
+            {
+                foodTitle:'Dosa',
+                price:40,
+                variant:['1 plate (2 dosas)','1 plate (2 Small dosas'],
+                imgUrl:'https://drive.google.com/file/d/1SGHpb7CsE0KQzboR2aho8tVjFd5baFUK/view?usp=sharing',
+            }
+        ]
+    },
+    {
+        title:'tifins',
+        status:"currently offline",
+        ratings:1,
+        offers:'Rs 100 CashBack',
+        opnings:"2 Days",
+        kms:7.6,
+        delivery:'free delivery',
+        foods:[
+            {
+                foodTitle:'Erra karam dosa With Bombay Chutney',
+                variant:null,
+                price:49,
+                imgUrl:'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/9/DV3016_masala-dosa_s4x3.jpg.rend.hgtvcom.616.462.suffix/1570635680579.jpeg',
+               
+            },
+            {
+                foodTitle:'Tomato Dosa ',
+                variant:null,
+                price:60,
+                imgUrl:'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/9/DV3016_masala-dosa_s4x3.jpg.rend.hgtvcom.616.462.suffix/1570635680579.jpeg',
+               
+            },
+            {
+                foodTitle:'butter paper dosa',
+                variant:null,
+                price:30,
+                imgUrl:'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/9/DV3016_masala-dosa_s4x3.jpg.rend.hgtvcom.616.462.suffix/1570635680579.jpeg',
+               
+            },
+            {
+                foodTitle:'chilli pancer Schezwan Dosa',
+                variant:null,
+                price:35,
+                imgUrl:'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/9/DV3016_masala-dosa_s4x3.jpg.rend.hgtvcom.616.462.suffix/1570635680579.jpeg',
+               
+            },
+        ]
+    },
+    {
+        title:'Mess',
+        status:"currently offline",
+        ratings:0,
+        offers:'Rs 100 CashBack',
+        opnings:'3 Hrs',
+        kms:9.5,
+        delivery:'free delivery',
+        foods:[
+            {
+                foodTitle:'All type of Dosa ',
+                variant:['1 peice'],
+                price:46,
+                imgUrl:url,
+            }
+        ]
+    }
+];
+export default data;
